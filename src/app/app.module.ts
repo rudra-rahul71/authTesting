14f1adc2cac0d282a51c1180a80b8c33d93e2907
Rahul <rudra.rahul71@gmail.com>
2022-11-08 21:35:27 -0500

    foundation of auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment as env } from 'src/environments/environment';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      ...env.auth,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
